54bc33d6d7bae046dc0e0fa372fcebb1e2f61ab4
Sonny <sonnygta@gmail.com>
2017-03-17 16:10:43 -0400

    my first commit

---
+++ first udemy.playground/Contents.swift
@@ -0,0 +1,21 @@
+var birthYear = 1976
+
+
+
+if birthYear < 2000 {
+    print("you were born in 20th century")
+} else {
+    print("you were born in 21st century")
+}
+
+
+
+var name = "sonny"
+
+if name == "sonny" {
+    print("we have the same name")
+    
+} else {
+print("we do not have the same")
+
+}
